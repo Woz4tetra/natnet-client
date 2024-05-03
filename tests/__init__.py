c9b94eb7fc7a1fb4ebd88c3e1935ec6b00845a6d
Ignacio de la Torre Arias <ignaciodlta@gmail.com>
2024-05-02 18:46:00 -0600

    First commit

---
+++ tests/__init__.py
@@ -0,0 +1,14 @@
+from natnet_client.Client import NatNetClient
+
+if __name__ == "__main__":
+  client = NatNetClient()
+  if client.connected:
+    print(client.server_info)
+    while input("Out? [N/y]").upper() != 'Y':
+      if client.mocap is not None:
+        print(client.mocap.rigid_body_data.rigid_bodies)
+      else:
+        print("Frame not received")
+    client.shutdown()
+  else:
+    print("Client didn't connect")
